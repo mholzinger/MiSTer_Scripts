6da7cd054019c3206af27109d5f3c84bb2d2c0a4
Mike Holzinger <mike.holzinger@gmail.com>
2020-06-15 19:11:55 -0700

    Adds parameter value check to see if other than default value days passed in

--- Scripts/whats_new.sh
+++ Scripts/whats_new.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# test for input
+# TODO: validate integer value to set input
+if [ -z "$1" ]
+  then
+    # Modified time in days:
+    mdays=2
+  else
+    mdays="$1"
+fi
+
 # Modified time in days:
 mdays=2
 
@@ -7,7 +17,7 @@ echo "Here is what's new in _Arcade in the last 48 hours:"
 find /media/fat/_Arcade/ -mtime -$mdays -iname "*mra"|\
 while read -r mra
   do
-    echo " -" `basename "$mra"` "["`date +"%a, %b %d" -r "$mra"`"]"
+    echo `basename "$mra"`
   done
 
 echo
@@ -15,6 +25,5 @@ echo "Here is what's new in compiled Mister Cores in the last 48 hours:"
 find /media/fat/ -mtime -$mdays -iname "*rbf"|\
   while read -r rbf
     do
-      echo " - in" `dirname "$rbf"` : `basename "$rbf"`  "["`date +"%a, %b %d" -r "$rbf"`"]"
+      echo Core in `dirname "$rbf"` : `basename "$rbf"`
     done
-
