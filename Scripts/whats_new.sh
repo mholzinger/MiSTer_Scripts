68def41cf43f45866176da9d57a3bd93d28599c0
Mike Holzinger <mike.holzinger@gmail.com>
2020-06-15 18:44:55 -0700

    Adds date + time to file output

--- Scripts/whats_new.sh
+++ Scripts/whats_new.sh
@@ -7,7 +7,7 @@ echo "Here is what's new in _Arcade in the last 48 hours:"
 find /media/fat/_Arcade/ -mtime -$mdays -iname "*mra"|\
 while read -r mra
   do
-    echo `basename "$mra"`
+    echo " -" `basename "$mra"` "["`date +"%a, %b %d" -r "$mra"`"]"
   done
 
 echo
@@ -15,6 +15,6 @@ echo "Here is what's new in compiled Mister Cores in the last 48 hours:"
 find /media/fat/ -mtime -$mdays -iname "*rbf"|\
   while read -r rbf
     do
-      echo Core in `dirname "$rbf"` : `basename "$rbf"`
+      echo " - in" `dirname "$rbf"` : `basename "$rbf"`  "["`date +"%a, %b %d" -r "$rbf"`"]"
     done
 
