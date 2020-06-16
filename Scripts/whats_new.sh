7a29821a8d242ae5f9a7b5b0465262faa5df0c8f
Mike Holzinger <mike.holzinger@gmail.com>
2020-06-15 19:20:13 -0700

    opps

--- Scripts/whats_new.sh
+++ Scripts/whats_new.sh
@@ -10,20 +10,21 @@ if [ -z "$1" ]
     mdays="$1"
 fi
 
-# Modified time in days:
-mdays=2
+mhours=$(($mdays * 24))
 
-echo "Here is what's new in _Arcade in the last 48 hours:"
+echo "Here is what's new in _Arcade in the last [$mhours] hours ($mdays days):"
 find /media/fat/_Arcade/ -mtime -$mdays -iname "*mra"|\
 while read -r mra
   do
-    echo `basename "$mra"`
+    echo " -" `basename "$mra"` "["`date +"%a, %b %d" -r "$mra"`"]"
   done
 
 echo
-echo "Here is what's new in compiled Mister Cores in the last 48 hours:"
+echo "Here is what's new in compiled Mister Cores in the last [$mhours] hours ($mdays days):"
 find /media/fat/ -mtime -$mdays -iname "*rbf"|\
   while read -r rbf
     do
-      echo Core in `dirname "$rbf"` : `basename "$rbf"`
+      echo " -" `dirname "$rbf"` : `basename $rbf` "["`date +"%a, %b %d" -r "$rbf"`"]"
     done
+
+
